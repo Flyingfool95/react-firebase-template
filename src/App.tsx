411aaf1aff7b5b0224a1bee6df1f42bbdb8ac0f1
Flyingfool95 <jhernehult@gmail.com>
2025-01-01 14:11:18 +0100

    setting up basic styling system

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-
-
 function App() {
     return (
         <>
-            <h1>Hello</h1>
+            <h1>React Firebase Template</h1>
+            <h2>React Firebase Template</h2>
+            <h3>React Firebase Template</h3>
+            <h4>React Firebase Template</h4>
+            <h5>React Firebase Template</h5>
+            <h6>React Firebase Template</h6>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis libero iure, placeat doloremque ex
+                rem alias maiores cupiditate dicta laboriosam blanditiis, porro reiciendis? Distinctio sint fuga tempora
+                repellendus iure natus in explicabo nostrum dolore corporis necessitatibus quae aliquid animi eius
+                accusantium, vitae atque perferendis velit quis accusamus nesciunt illum? Saepe minima error distinctio?
+                Explicabo reiciendis aliquam in voluptas aliquid dicta ducimus eligendi voluptates odit quia adipisci
+                perspiciatis fugit est ullam ut ea similique provident, accusamus illo? Esse quisquam aliquam reiciendis
+                ab deserunt, molestiae maxime quas voluptas veniam doloremque excepturi repellendus omnis aut neque
+                velit odio quidem labore eos minima corporis!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere nobis excepturi autem cumque aliquid{" "}
+                <span>reprehenderit non vitae eligendi delectus totam debitis magni odio architecto suscipit</span>{" "}
+                assumenda repellat, ad dignissimos rerum voluptatem ipsam eius culpa distinctio incidunt. Accusantium
+                totam tempora illo eligendi, excepturi earum magni sit culpa adipisci est consectetur ad!
+            </p>
         </>
     );
 }
