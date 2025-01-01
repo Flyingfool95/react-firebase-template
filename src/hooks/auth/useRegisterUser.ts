7fc5b4aa1c077c5a818540aeadbd61bd047c5708
Flyingfool95 <jhernehult@gmail.com>
2025-01-01 19:31:09 +0100

    removing Zod for now

--- src/hooks/auth/useRegisterUser.ts
+++ src/hooks/auth/useRegisterUser.ts
@@ -2,6 +2,7 @@ import { createUserWithEmailAndPassword } from "firebase/auth/cordova";
 import { auth } from "../../services/firebase/firebase";
 
 export default function useRegisterUser() {
+
     const registerUser = async (email: string, password: string, confirmPassword: string) => {
         if (!email || !password || !confirmPassword) return console.error("All fields must be filled");
         if (password !== confirmPassword) return console.error("Passwords must match");
