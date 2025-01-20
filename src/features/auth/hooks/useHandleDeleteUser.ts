fa4f65fc1abf1b719cce32222763abfed52f1dd8
Flyingfool95 <jhernehult@gmail.com>
2025-01-20 13:55:15 +0100

    Finishing default useHandleDeleteUser

--- src/features/auth/hooks/useHandleDeleteUser.ts
+++ src/features/auth/hooks/useHandleDeleteUser.ts
@@ -4,7 +4,24 @@ import { auth } from "../../../services/firebase/firebase";
 export default function useHandleDeleteUser() {
     const [deleteUser, loading, error] = useDeleteUser(auth);
 
-    const handleDeleteUser = () => {};
+    if (error) {
+        //Add error to toast
+        console.error(error);
+    }
+
+    if (loading) {
+        throw new Promise(() => {});
+    }
+
+    const handleDeleteUser = async () => {
+        //Delete all data belonging to user here first
+
+        const success = await deleteUser();
+
+        if (success) {
+            console.log("User deleted...");
+        }
+    };
 
     return {
         handleDeleteUser,
