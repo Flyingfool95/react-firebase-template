dfb66127acb1f9aad8daac4f944b3be882b753ec
Flyingfool95 <jhernehult@gmail.com>
2025-01-20 14:01:06 +0100

    Adding delete me button on dashboard

--- src/features/dashboard/routes/Dashboard.tsx
+++ src/features/dashboard/routes/Dashboard.tsx
@@ -1,11 +1,14 @@
 import { useAuthState, useSignOut } from "react-firebase-hooks/auth";
 import { auth } from "../../../services/firebase/firebase";
 import { useNavigate } from "react-router";
+import useHandleDeleteUser from "../../auth/hooks/useHandleDeleteUser";
 
 export default function Dashboard() {
     const [signOut, loading, error] = useSignOut(auth);
     const [user] = useAuthState(auth);
     const navigate = useNavigate();
+
+    const {handleDeleteUser} = useHandleDeleteUser();
 
     const handleSignout = () => {
         signOut();
@@ -16,6 +19,7 @@ export default function Dashboard() {
         <div>
             <h1>Welcome {user?.email}</h1>
             <button onClick={() => handleSignout()}>Logout</button>
+            <button onClick={() => handleDeleteUser()}>Delete me!</button>
         </div>
     );
 }
